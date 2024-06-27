cf5de31d6600f1e1d3266b08c24b099368d05944
krgarav <krgarav@gmail.com>
2024-06-27 15:06:21 +0530

    Added db table

--- utils/database.js
+++ utils/database.js
@@ -1,23 +1,23 @@
 const { Sequelize } = require("sequelize");
 
-// const sequelize = new Sequelize(
-//   "triangleindia_newtable",
-//   "triangleindia_gaurav",
-//   "9800664253Kg@",
-//   {
-//     dialect: "mysql",
-//     host: "triangleindiatours.com",
-//     port: 3306, // Ensure this is the correct port
-//     logging: false,
-//   }
-// );
+const sequelize = new Sequelize(
+  "triangleindia_tours",
+  "triangleindia_tours",
+  "CuE2JpGaYhM4",
+  {
+    dialect: "mysql",
+    host: "triangleindiatours.com",
+    port: 3306, // Ensure this is the correct port
+    logging: false,
+  }
+);
 
-const sequelize = new Sequelize("triangleindia_tour", "root", "root", {
-  dialect: "mysql",
-  host: "localhost",
-  port: 3306, // Ensure this is the correct port
-  logging: false,
-});
+// const sequelize = new Sequelize("triangleindia_tour", "root", "root", {
+//   dialect: "mysql",
+//   host: "localhost",
+//   port: 3306, // Ensure this is the correct port
+//   logging: false,
+// });
 
 
 
