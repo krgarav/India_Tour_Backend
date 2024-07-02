98006ed803d97fd210a3eb5f08cc99c4725d8ef3
krgarav <krgarav@gmail.com>
2024-07-02 11:43:26 +0530

    New Build file updated

--- utils/database.js
+++ utils/database.js
@@ -1,16 +1,16 @@
 const { Sequelize } = require("sequelize");
 
-// const sequelize = new Sequelize(
-//   "triangleindia_tours",
-//   "triangleindia_tours",
-//   "CuE2JpGaYhM4",
-//   {
-//     dialect: "mysql",
-//     host: "triangleindiatours.com",
-//     port: 3306, // Ensure this is the correct port
-//     logging: false,
-//   }
-// );
+const sequelize = new Sequelize(
+  "triangleindia_tours",
+  "triangleindia_tours",
+  "CuE2JpGaYhM4",
+  {
+    dialect: "mysql",
+    host: "triangleindiatours.com",
+    port: 3306, // Ensure this is the correct port
+    logging: false,
+  }
+);
 
 // const sequelize = new Sequelize("triangleindia_tour", "root", "root", {
 //   dialect: "mysql",
@@ -18,12 +18,12 @@ const { Sequelize } = require("sequelize");
 //   port: 3306, // Ensure this is the correct port
 //   logging: false,
 // });
-const sequelize = new Sequelize("triangleindia_tour", "gaurav", "9800664253", {
-  dialect: "mysql",
-  host: "database-1.cpjefdwfpvhf.ap-south-1.rds.amazonaws.com",
-  port: 3306, // Ensure this is the correct port
-  logging: false,
-});
+// const sequelize = new Sequelize("triangleindia_tour", "gaurav", "9800664253", {
+//   dialect: "mysql",
+//   host: "database-1.cpjefdwfpvhf.ap-south-1.rds.amazonaws.com",
+//   port: 3306, // Ensure this is the correct port
+//   logging: false,
+// });
 
 
 
