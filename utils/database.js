ede8ce1b1e2a4b5771b7e00432f04a727e47f083
krgarav <krgarav@gmail.com>
2024-07-02 09:58:43 +0530

    new DB added

--- utils/database.js
+++ utils/database.js
@@ -12,9 +12,15 @@ const { Sequelize } = require("sequelize");
 //   }
 // );
 
-const sequelize = new Sequelize("triangleindia_tour", "root", "root", {
+// const sequelize = new Sequelize("triangleindia_tour", "root", "root", {
+//   dialect: "mysql",
+//   host: "localhost",
+//   port: 3306, // Ensure this is the correct port
+//   logging: false,
+// });
+const sequelize = new Sequelize("triangleindia_tour", "gaurav", "9800664253", {
   dialect: "mysql",
-  host: "localhost",
+  host: "database-1.cpjefdwfpvhf.ap-south-1.rds.amazonaws.com",
   port: 3306, // Ensure this is the correct port
   logging: false,
 });
