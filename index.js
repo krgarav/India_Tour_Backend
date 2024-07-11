6d78ef6a23be4aae6d3b876147cd4a5bf653871f
krgarav <krgarav@gmail.com>
2024-07-11 19:53:20 +0530

    Change build file for backend

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const https = require("https");
 // const http = require("http");
 
 const fs = require("fs");
-// const http = require("http");
+const http = require("http");
 const sequelize = require("./utils/database");
 const User = require("./models/authSchema");
 const Tour = require("./models/tourSchema");
@@ -95,12 +95,12 @@ sequelize
     }
 
     // // Read SSL certificate and key files
-    const options = {
-      key: fs.readFileSync("/etc/letsencrypt/live/triangleindiatour.uk.to/privkey.pem"), // Replace with your private key file path
-      cert: fs.readFileSync("/etc/letsencrypt/live/triangleindiatour.uk.to/fullchain.pem") // Replace with your fullchain.pem file path
-    };
+    // const options = {
+    //   key: fs.readFileSync("/etc/letsencrypt/live/triangleindiatour.uk.to/privkey.pem"), // Replace with your private key file path
+    //   cert: fs.readFileSync("/etc/letsencrypt/live/triangleindiatour.uk.to/fullchain.pem") // Replace with your fullchain.pem file path
+    // };
 
-    https.createServer(options,app).listen(PORT, () => {
+    http.createServer(app).listen(PORT, () => {
       console.log(`HTTPS Server is running on port ${PORT}`);
     });
   })
