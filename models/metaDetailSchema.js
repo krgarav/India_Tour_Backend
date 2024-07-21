088ca9ca01b981bd45c214eae646c10af8bdd83e
krgarav <krgarav@gmail.com>
2024-07-21 23:54:04 +0530

    Meta detail schema added

---
+++ models/metaDetailSchema.js
@@ -0,0 +1,36 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../utils/database");
+const Tour = require("./tourSchema");
+
+const TourData = sequelize.define("TourData", {
+  fullDescription: {
+    type: DataTypes.TEXT,
+  },
+  luxuryHotel: {
+    type: DataTypes.BOOLEAN,
+  },
+  wifi: {
+    type: DataTypes.BOOLEAN,
+  },
+  transport: {
+    type: DataTypes.BOOLEAN,
+  },
+  fooding: {
+    type: DataTypes.BOOLEAN,
+  },
+  others: {
+    type: DataTypes.STRING,
+  },
+});
+
+Tour.hasOne(TourData, {
+  foreignKey: "tourId",
+  onDelete: "CASCADE",
+  onUpdate: "CASCADE",
+});
+TourData.belongsTo(Tour, {
+//   foreignKey: "tourId",
+  onUpdate: "CASCADE",
+});
+
+// module.exports = TourData;
