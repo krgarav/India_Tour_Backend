bf2c45b424edfbf8c62357412b4bba039f28013e
shivam singh <ssultanpura9@gmail.com>
2024-07-28 22:43:49 +0530

    added slider img controllers for adding,editing,deliting and getting dsta

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -13,7 +13,7 @@ const {
   addToHomePageSection,
   removeHomePageSection,
 } = require("../controllers/tourController");
-const { addSliderData } = require("../controllers/homePageController");
+const { addSliderData, deleteSliderData, editSliderData, getAllSliderData } = require("../controllers/homePageController");
 
 router.post("/createtour", createTour);
 router.get("/fetchhomepagesection", fetchHomePageSection);
@@ -24,6 +24,8 @@ router.get("/tours", getAllTours);
 router.get("/top/tours", getAllTopTours);
 router.put("/tour/edit/:id", editTour); //tourId
 router.delete("/tour/delete/:id", deleteTour); //tourId
-router.post("addSliderData",upload.single("sliderImg"),addSliderData)
-
+router.post("/addSliderData",upload.single("sliderImg"),addSliderData)
+router.delete("/deleteSliderData/:id",deleteSliderData)
+router.put("/editSliderData",upload.single("sliderImg"),editSliderData)
+router.get("/getSliderData",getAllSliderData)
 module.exports = router;
