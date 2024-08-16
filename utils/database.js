46d3e4106dd526bfdf6367244390d4b1a8d07e89
krgarav <krgarav@gmail.com>
2024-08-16 23:21:18 +0530

    Added tour highlights

--- utils/database.js
+++ utils/database.js
@@ -12,25 +12,25 @@ const { Sequelize } = require("sequelize");
 //   }
 // );
 
-const sequelize = new Sequelize("triangleindia_tour", "root", "root", {
-  dialect: "mysql",
-  host: "localhost",
-  port: 3306, // Ensure this is the correct port
-  logging: false,
-});
+// const sequelize = new Sequelize("triangleindia_tour", "root", "root", {
+//   dialect: "mysql",
+//   host: "localhost",
+//   port: 3306, // Ensure this is the correct port
+//   logging: false,
+// });
 
 
-// const sequelize = new Sequelize(
-//   "triangleindia_tours",
-//   "triangleindia_tours",
-//   "CuE2JpGaYhM4",
-//   {
-//     dialect: "mysql",
-//     host: "triangleindiatours.com",
-//     port: 3306, // Ensure this is the correct port
-//     logging: false,
-//   }
-// );
+const sequelize = new Sequelize(
+  "triangleindia_tours",
+  "triangleindia_tours",
+  "CuE2JpGaYhM4",
+  {
+    dialect: "mysql",
+    host: "triangleindiatours.com",
+    port: 3306, // Ensure this is the correct port
+    logging: false,
+  }
+);
 
 
 
