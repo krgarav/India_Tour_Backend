d8c3019e20b8fca5ee2eb6800fb3d4ebb382c668
Kumar Gaurav <76562785+krgarav@users.noreply.github.com>
2025-02-08 15:40:32 +0530

    configured db for hosting

--- utils/database.js
+++ utils/database.js
@@ -1,5 +1,8 @@
 const { Sequelize } = require("sequelize");
-
+const path = require("path");
+const dotenv = require("dotenv");
+const fs = require('fs');
+dotenv.config();
 // const sequelize = new Sequelize(
 //   "triangleindia_tours",
 //   "root",
@@ -20,18 +23,31 @@ const { Sequelize } = require("sequelize");
 // });
 
 
-const sequelize = new Sequelize(
-  "triangleindia_tours",
-  "triangleindia_tours",
-  "CuE2JpGaYhM4",
-  {
-    dialect: "mysql",
-    host: "triangleindiatours.com",
-    port: 3306, // Ensure this is the correct port
-    logging: false,
-  }
-);
+// const sequelize = new Sequelize(
+//   "triangleindia_tours",
+//   "triangleindia_tours",
+//   "CuE2JpGaYhM4",
+//   {
+//     dialect: "mysql",
+//     host: "triangleindiatours.com",
+//     port: 3306, // Ensure this is the correct port
+//     logging: false,
+//   }
+// );
 
+const caCertPath = path.join(__dirname, "../", "ca.pem");
+const caCert = fs.readFileSync(caCertPath);
 
+// Sequelize Configuration
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DBUSERNAME, process.env.DBPASSWORD, {
+    host: process.env.DBHOSTNAME,
+    dialect: 'mysql',
+    port: 26370,
+    dialectOptions: {
+        ssl: {
+            ca: caCert
+        }
+    }
+});
 
 module.exports = sequelize;
